7f3ed5fcfef79892b6ffd321f79742d621e71f61
anton <antonio.kolesnikow@gmail.com>
2026-02-13 14:52:52 +0300

    Added simple processing of text from file

--- Parser/backend/main.go
+++ Parser/backend/main.go
@@ -1,1 +1,34 @@
-package backend
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"path/filepath"
+)
+
+func main() {
+
+	// dictionary := map[string]int{
+	// 	"==": 0,
+	// 	"!=": 0,
+	// }
+
+	filepath := filepath.Join("..", "..", "ExampleCode", "main.go")
+
+	file, error := os.Open(filepath)
+
+	if error != nil {
+		log.Fatal(error)
+	}
+
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	scanner.Split(bufio.ScanWords)
+
+	for scanner.Scan() {
+		fmt.Println(scanner.Text())
+	}
+}
