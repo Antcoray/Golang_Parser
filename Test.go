add7de8c6cbbfea3be14a4601f50babff12dea43
vladozavr2006 <vladozavr2006@mail.ru>
2026-02-23 21:44:00 +0300

    Add files via upload
    Final test

--- Test.go
+++ Test.go
@@ -5,7 +5,7 @@ import (
 	"math"
 )
 
-func Test() {
+func main() {
 	fmt.Println("Pizza")
 	var x int = 15
 	var y int = 20
@@ -22,7 +22,12 @@ func Test() {
 		fmt.Println("aboba")
 	}
 	for a := 0; a < 10; a++ {
-		fmt.Println(a)
+		for u := 5; u < 10; u++ {
+			fmt.Println(a * u)
+			if 8 == 8 {
+				break
+			}
+		}
 	}
 	x++
 	x = x + y - 4*5
@@ -32,10 +37,9 @@ L1:
 	a--
 	switch a {
 	case 1:
-		{
-			aboba := "aboba"
-			fmt.Println([]byte(aboba))
-		}
+		aboba := "aboba"
+		fmt.Println([]byte(aboba))
+		fallthrough
 	case 2:
 		{
 			goto L1
@@ -49,4 +53,38 @@ L1:
 			goto L1
 		}
 	}
+	fmt.Println(a)
+	var numbers [500]float64 = [500]float64{1, 2.0, 3.0, 4.0, 5.0}
+	fmt.Println(len(numbers))
+	for _, value := range numbers {
+		if value != 0 {
+			fmt.Println(value)
+		}
+	}
+	fmt.Println(numbers)
+	fmt.Println(Add(int(numbers[0]), int(numbers[1]), int(numbers[5])))
+	fmt.Println(Multiply(1, 2, 3))
+	fmt.Println(Multiply(1, 2, 3, 4))
+	var p *int = &x
+	fmt.Println(*p&45*3/78 | 4)
+	var b = 45 &^ 6 & 8932 << 4
+	fmt.Println(b)
+	type chuvak struct {
+		Age  int
+		Cool bool
+		Cash int
+		Name string
+	}
+	var Chuvachok chuvak = chuvak{18, false, 0, "Durak"}
+	fmt.Println(Chuvachok)
+}
+func Add(x1 int, x2 int, x3 int) int {
+	return x1 + x2 + x3
+}
+func Multiply(numbers ...float64) float64 {
+	var sum float64 = 1
+	for _, number := range numbers {
+		sum *= number
+	}
+	return sum
 }
